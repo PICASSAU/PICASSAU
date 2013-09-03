148a80f3c9cbe1e8e55688d029353697cab3d1fa
Ben Straub <straub@auburn.edu>
2013-09-02 19:47:11 -0500

    wrote most of plotting and fixed up handshake
    still needs:
    - accurate numbers in header file
    - scaling on the plotting coordinates
    - testing with python
    - all brush-related stuff

--- picassau/handshake.ino
+++ picassau/handshake.ino
@@ -1,56 +1,87 @@
-String inputinstruction = "";
-verificationbyte = 0;
+char stringBuffer[25];
+byte stringIndex = 0;
 
+void serialSetup() //initialize serial port
+{
+  Serial.begin(9600); //open serial port, set data rate to 9600 bps
+  stringBuffer[0] = '\0';
+  stringIndex = 0;
+}
 
-void setup() //initialize serial port
-    {
-        Serial.begin(9600); //open serial port, set data rate to 9600 bps
-        inputinstruction.reserve(25); //reserve 25 bytes for string
-    }
+void serialReady() //Send ready status to computer
+{
+  while(Serial.available()){Serial.read();} //Serial.flush
+  Serial.print("R");    //when ready, inform computer
+  stringBuffer[0] = '\0';
+  stringIndex = 0;
+}
 
-
-void ready() //Send ready status to computer
-    {
-        Serial.print("R");    //when ready, inform computer
-    }    
+void serialError()
+{
+  Serial.print("X");  //error in reading message
+}
     
 void receiveInstruction() //receive instruction from computer
-    {
-        while (!Serial.available()) {};  //wait until data in serial buffer
-        inputinstruction = Serial.read();   //write buffer data into string
-    }
-    
-boolean verifyInstruction()  //check if correct instr. received with computer
-    {
-        Serial.print(inputinstruction); //send received instr. back to comp.
-        while (!Serial.available()) {}; //wait for verification
-        verificationbyte = Serial.read(); //write verification status to variable
-        return (verificationbyte != 'G') //if the instruction received isn't correct
-    }
-     
-void ExecuteInstruction() //this program executes instruction received by Ard.      
-    {
-         /////////// ******execute instruction code;****///////////
-         verificationbyte = 0; //reset verification bit
-         inputinstruction = ""; //clear instruction string
-       } 
-
-//////////////////////////////MAIN//////////////////////////////////////////////
-void loop()
 {
-    setup(); 
-    while(1); 
-       {
-         status(); //Transmit status
-         do 
-         {
-            receiveInstruction(); //Receive instruction
-         } while ( !verifyInstruction() ) //verify instruction
-         ExecuteInstruction(); // execute
-       } //repeat until done painting
-      
-////////////////////////////////////////////////////////////////////////////////      
-        
-        
+  //Serial.println("receiving");
+  char temp;
+  //Serial.println(stringBuffer);
+  do
+  {
+    while (!Serial.available()) {};  //wait until data in serial buffer
+    temp = Serial.read();   //write buffer data into string
+    Serial.print(temp);
+    stringBuffer[stringIndex++] = temp;
+  } while (temp != '\n');
+  stringBuffer[stringIndex] = '\0';
+  //Serial.print(stringBuffer[0]);
+  //Serial.print(stringBuffer);
+}
+    
+boolean verifyInstruction()  //check if correct instr. received with computer and parse numbers out
+{
+  while(Serial.available()){Serial.read();} //Serial.flush - make sure there's nothing sitting in the Serial buffer
+  Serial.print(stringBuffer); //send received instr. back to comp.
+  while (!Serial.available()) {} //wait for verification
+  stringBuffer[0] = Serial.read(); //write verification status to buffer
+  stringIndex = 1;
+  stringBuffer[1] = '\0';
+  if(stringBuffer[0] != 'G') //if the instruction received isn't correct
+    return false; 
+  return true;
+}
+
+boolean parseInstruction()
+{
+  command = stringBuffer[0];
+  if ((command != 'L') && (command != 'M'))
+    return false;
+  if (stringBuffer[1] != ' ')
+    return false;
+  
+  char i = 2;
+  int temp = 0;
+  while (stringBuffer[i] != ',') //keep looping until you get to a comma
+  {
+    if ((stringBuffer[i] < 0x30) || (stringBuffer[i] > 0x39))
+      return false; //not a number
+    temp = temp*10 + stringBuffer[i] - 0x30;
+    if ((++i) > 12) //allows a max of 10 digits
+      return false; //too long
+  }
+  cDest.x = temp;
+  i++;
+  temp = 0;
+  while (stringBuffer[i] != '\n') //keep looping until you get to a comma
+  {
+    if ((stringBuffer[i] < 0x30) || (stringBuffer[i] > 0x39))
+      return false; //not a number
+    temp = temp*10 + stringBuffer[i] - 0x30;
+    if ((++i) > 23) //allows a max of 10 digits
+      return false; //too long
+  }
+  cDest.y = temp;
+  return true;
+}
         
         
--- picassau/picassau.h
+++ picassau/picassau.h
@@ -1,4 +1,19 @@
 
 typedef struct Coord { float x; float y; } coord;
 
-const float X2 = 123; // !! fill this in after building
+const float MOTOR_DIST = 280; // how far are the motors apart?
+const float DBL_MOTOR_DIST = 2*MOTOR_DIST;
+const float MOTOR_DIST2 = MOTOR_DIST*MOTOR_DIST;
+
+const int MOTOR_DELAY = 20; // in ms, time between stepping the motors
+
+const float INITIAL_LENGTH_L = 330; //based on the starting position
+const float INITIAL_LENGTH_R = 330; // of the platform
+
+const float CIRCUM = 11.125; //in inches, circumference of the spindle
+const int NSTEPS = 100; //number of steps per revolution of the stepper motors
+const float STEP_DIST = 1;//CIRCUM/NSTEPS; //the change in distance between motor and platform when motor steps
+
+// (0,0) is at the left spindle
+const int COORD_OFFSET_X = 50; //so at what coordinates is the top left
+const int COORD_OFFSET_Y = 150; // corner of the canvas?
