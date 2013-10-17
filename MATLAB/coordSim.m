479bfdeb9a2069beb87a1ffac836c3493e3e5d4c
PeterG <cheflongtoe@gmail.com>
2013-10-17 11:36:54 -0500

    Update coordSim.m
    we can play around with the line properties, i.e. color, width, etc. ergo... vis a vis... voila!

--- MATLAB/coordSim.m
+++ MATLAB/coordSim.m
@@ -4,9 +4,11 @@
 % Ben Straub, David Toledo, Drew Kerr, Kayla Frost, and Peter Gartland
 % 6 Oct. 2013
 
+
 % This MATLAB script will read a file containing the coordinates and move
 % commands used by PICASSAU and will generate a plot, so we can see what
 % PICASSAU is about to paint.
+
 
 %{
 % define some useful variables, arrays, etc.
@@ -15,10 +17,10 @@ xcd0;  %list of x-coordinates for color 0
 ycd0;  %list of y-coordinates for color 0
 cmd1;  %list of commands for color 1
 xcd1;  %list of x-coordinates for color 1
-ycd0;  %list of y-coordinates for color 1
+ycd1;  %list of y-coordinates for color 1
 cmd2;  %list of commands for color 2
-xcd0;  %list of x-coordinates for color 2
-ycd0;  %list of y-coordinates for color 2
+xcd2;  %list of x-coordinates for color 2
+ycd2;  %list of y-coordinates for color 2
 %}
 
 %clear workspace
@@ -30,67 +32,117 @@ pyOutFile = fopen('pythonOutput.txt');
 %=================
 % get color 0 info
 %=================
-%extract color 0 commands
+%get commands
 pyLine = fgetl(pyOutFile);  %get a line from the file
-pyLineData = strtok(pyLineData, '[]');  %get everything inside the brackets, [...]
-data = strsplit(pyLineData, ',');  %get data from in between commas
-cmd0 = char(data);  %convert cell array (of strings) into a character array
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+pyLineData = strsplit(pyLineData, ',');  %remove commas
+cmd0 = char(pyLineData);  %turn cell array (of strings) into a char array
 
-%{
-pyLineDatum = sscanf(pyLineData, '%c');  %look at one charcter at a time
-while pyLineDatum ~= ']'
-    if pyLineDatum ~= ','
-        cmd0(i) = pyLineDatum;
-    end
-    i = i + 1;
-    pyLineDatum = sscanf(pyLineData, '%c');  %get the next character
-end
+%get x-coords
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+xcd0 = str2num(pyLineData);  %turn string into numeric array
+xcd0 = transpose(xcd0);  %it's easier to work with column vectors
 
-%extract color 0 x-coords
-pyLine = fgetl(pyOutFile)
-lineDesc = sscanf(pyLine, '%s')
-[xcd0, xcd0_size] = sscanf(pyLine, '%d')
-
-%extract color 0 y-coords
+%get y-coords
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+ycd0 = str2num(pyLineData);  %turn string into numeric array
+ycd0 = transpose(ycd0);  %it's easier to work with column vectors
+ycd0 = -1.*ycd0;  %negate y-coords because our coord system is upside down
 
 
-	
 %=================
 % get color 1 info
 %=================
-pyLine = fgetl(pyOutFile);
-%extract color 1 commands
+%get commands
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+pyLineData = strsplit(pyLineData, ',');  %remove commas
+cmd1 = char(pyLineData);  %turn cell array (of strings) into a char array
 
-pyLine = fgetl(pyOutFile);
-%extract color 1 x-coords
+%get x-coords
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+xcd1 = str2num(pyLineData);  %turn string into numeric array
+xcd1 = transpose(xcd1);  %it's easier to work with column vectors
 
-pyLine = fgetl(pyOutFile);
-%extract color 1 y-coords
-
-
+%get y-coords
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+ycd1 = str2num(pyLineData);  %turn string into numeric array
+ycd1 = transpose(ycd1);  %it's easier to work with column vectors
+ycd1 = -1.*ycd1;  %negate y-coords because our coord system is upside down
 
 %=================
 % get color 2 info
 %=================
-pyLine = fgetl(pyOutFile);
-%extract color 2 commands
+%get commands
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+pyLineData = strsplit(pyLineData, ',');  %remove commas
+cmd2 = char(pyLineData);  %turn cell array (of strings) into a char array
 
-pyLine = fgetl(pyOutFile);
-%extract color 2 x-coords
+%get x-coords
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+xcd2 = str2num(pyLineData);  %turn string into numeric array
+xcd2 = transpose(xcd2);  %it's easier to work with column vectors
 
-pyLine = fgetl(pyOutFile);
-%extract color 2 y-coords
+%get y-coords
+pyLine = fgetl(pyOutFile);  %get a line from the file
+[pyLineDesc, pyLineData] = strtok(pyLine, '[');  %get bracketed info
+pyLineData = strtok(pyLineData, '[]');  %remove brackets
+ycd2 = str2num(pyLineData);  %turn string into numeric array
+ycd2 = transpose(ycd2);  %it's easier to work with column vectors
+ycd2 = -1.*ycd2;  %negate y-coords because our coord system is upside down
 
-
-
-%three for-loops to plot the three colors
+%=========================================
+% three for-loops to plot the three colors
+%=========================================
 %for-loop for color 0
-
+if length(cmd0) > 1
+    for i=2:length(cmd0)
+        if cmd0(i) == 'L'
+            seg = plot([xcd0(i-1) xcd0(i)],[ycd0(i-1) ycd0(i)]);
+            hold on;  %retain current graph while adding line segments
+            set(seg,'Color',[0,0,.50]);  %dark blue
+            set(seg,'LineWidth',10);  %default is 0.5
+            %set(seg,'Marker','x');  %makes counting line segments easier
+        end
+    end
+end
 
 %for-loop for color 1
-
+if length(cmd1) > 1
+    for i=2:length(cmd1)
+        if cmd1(i) == 'L'
+            seg = plot([xcd1(i-1) xcd1(i)],[ycd1(i-1) ycd1(i)]);
+            hold on;  %retain current graph while adding line segments
+            set(seg,'Color',[.25,.41,.88]);  %light blue
+            set(seg,'LineWidth',10);  %default is 0.5
+            set(seg,'Marker','.');  %makes counting line segments easier
+        end
+    end
+end
 
 %for-loop for color 2
-
-
-%}
+if length(cmd2) > 1
+    for i=2:length(cmd2)
+        if cmd2(i) == 'L'
+            seg = plot([xcd2(i-1) xcd2(i)],[ycd2(i-1) ycd2(i)]);
+            hold on;  %retain current graph while adding line segments
+            set(seg,'Color',[1,.65,0]);  %burnt orange
+            set(seg,'LineWidth',10);  %default is 0.5
+            set(seg,'Marker','.');  %makes counting line segments easier
+        end
+    end
+end
