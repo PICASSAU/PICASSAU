84cb389607b7afa096549b2cffb4f17ef0ad033f
Kayla Frost <kmf0008@auburn.edu>
2013-11-04 18:23:14 -0600

    Initial stab at GUI design
    Need to test on RPi!!!!!!!!!!!!!!!!!!!

---
+++ Python/GUI.py
@@ -0,0 +1,59 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Kayla
+#
+# Created:     04/11/2013
+# Copyright:   (c) Kayla 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from PIL import Image, ImageTk
+import Tkinter as Tk
+
+
+class Example(Tk.Frame):
+
+    def __init__(self, parent):
+        Tk.Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.initPic()
+
+    def initPic(self):
+
+        imageName = "../imageFiltering/teamTestPic.png"
+
+        self.parent.title("Label")
+
+        self.img = Image.open(imageName)
+        self.filteredImage = ImageTk.PhotoImage(self.img)
+        labelImage = Tk.Label(image=self.filteredImage)
+
+        labelImage.image = self.filteredImage
+        labelImage.grid(row = 1, column = 0)
+
+
+    def setGeometry(self, root):
+
+        w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+        root.geometry("%dx%d+0+0" % (w, h))
+
+
+def main():
+
+    root = Tk.Tk()
+    ex = Example(root)
+    text1 = Tk.Label(root, text="Take Picture")
+    text1.grid(row = 0, column = 1)
+
+    text2 = Tk.Label(root, text="Continue")
+    text2.grid(row = 2, column = 1)
+    ex.setGeometry(root)
+
+    root.mainloop()
+
+
+
+if __name__ == '__main__':
+    main()
