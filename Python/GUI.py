df5ce32970e5f5dc04e8c7883bfa1253a49b6f7e
Kayla Frost <kmf0008@auburn.edu>
2013-11-10 21:13:36 -0600

    Added colors to the GUI!

--- Python/GUI.py
+++ Python/GUI.py
@@ -24,8 +24,8 @@ class Example(Tk.Frame):
 
         imageName = "../imageFiltering/webcam2.png"
 
-        self.xmax = 506 
-        self.ymax = 379 
+        self.xmax = 506
+        self.ymax = 379
         self.cropConstant = (20.0/26.0)
         self.croppedX = int(self.ymax*self.cropConstant)
         box = (0, 0, self.croppedX, self.ymax)
@@ -36,7 +36,7 @@ class Example(Tk.Frame):
         self.img = Image.open(imageName)
         self.imgCropped = self.img.crop(box)
         self.filteredImage = ImageTk.PhotoImage(self.imgCropped)
-        labelImage = Tk.Label(image=self.filteredImage)
+        labelImage = Tk.Label(image=self.filteredImage, background='blue')
 
         labelImage.image = self.filteredImage
         labelImage.grid(row = 1, column = 0)
@@ -46,16 +46,17 @@ class Example(Tk.Frame):
 
         w, h = root.winfo_screenwidth(), root.winfo_screenheight()
         root.geometry("%dx%d+0+0" % (w, h))
+        root.configure(background='blue')
 
 
 def main():
 
     root = Tk.Tk()
     ex = Example(root)
-    text1 = Tk.Label(root, text="         Take Picture ", font=("Helvetica", 20, "bold"))
+    text1 = Tk.Label(root, text="         Take Picture ", font=("Helvetica", 20, "bold"), fg='orange', bg = 'blue')
     text1.grid(row = 0, column = 2)
 
-    text2 = Tk.Label(root, text="         Continue ", font=("Helvetica", 20, "bold"))
+    text2 = Tk.Label(root, text="         Continue ", font=("Helvetica", 20, "bold"), fg='orange', bg = 'blue')
     text2.grid(row = 2, column = 2)
     ex.setGeometry(root)
 
