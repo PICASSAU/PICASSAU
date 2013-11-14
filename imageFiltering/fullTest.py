65b7c21ebb6b5112b94c4c5281aa5f515df66cb9
Ben Straub <straub@auburn.edu>
2013-11-13 21:55:14 -0600

    have the full image processing + tracing + writing to an output file written
    I still need to see if it runs and works.  Expect fixes to follow in the
    near future.

--- imageFiltering/fullTest.py
+++ imageFiltering/fullTest.py
@@ -41,6 +41,7 @@ class Tracer():
         
         self.scaleFactorX = canvasX*ardDist/imgX
         self.scaleFactorY = canvasY*ardDist/imgY
+        
     
     #addArray takes an array of points and its it to the command and
 	# coordinate arrays.
@@ -143,25 +144,81 @@ class Tracer():
         imBin2[img == 85] = 255
         imBin3[img == 0] = 255
 
-        traceBin( imBin1, 0 )
-        traceBin( imBin2, 1 )
-        traceBin( imBin3, 2 )
+        self.traceBin( imBin1, 0 )
+        self.traceBin( imBin2, 1 )
+        self.traceBin( imBin3, 2 )
+        
+    def clearArrays(self):
+        self.commands0 = ['C']
+        self.xCoords0 = [0]
+        self.yCoords0 = [0]
+
+        self.commands1 = ['C']
+        self.xCoords1 = [1]
+        self.yCoords1 = [0]
+
+        self.commands2 = ['C']
+        self.xCoords2 = [2]
+        self.yCoords2 = [0]
+        
+    def writeArray(self, file, list):
+		file.write("[")
+		strList = [str(i) for i in list]
+		file.write(','.join(strList))
+		file.write("]\n")
+    
+    def writeFile(self, fileName)
+        if os.path.isfile(outputFileName):
+            os.remove(outputFileName)
+        file = open(outputFileName, 'w')
+
+        file.write("commands0 = ")
+        myTracer.writeToFile(file, myTracer.commands)
+        file.write("xcoords0 = ")
+        myTracer.writeToFile(file, myTracer.xcoords)
+        file.write("ycoords0 = ")
+        myTracer.writeToFile(file, myTracer.ycoords)
+        
+        file.write("commands1 = ")
+        mySVG.writeToFile(file, mySVG.commands1)
+        file.write("xCoords1 = ")
+        mySVG.writeToFile(file, mySVG.xCoords1)
+        file.write("yCoords1 = ")
+        mySVG.writeToFile(file, mySVG.yCoords1)
+        
+        file.write("commands2 = ")
+        mySVG.writeToFile(file, mySVG.commands2)
+        file.write("xCoords2 = ")
+        mySVG.writeToFile(file, mySVG.xCoords2)
+        file.write("yCoords2 = ")
+        mySVG.writeToFile(file, mySVG.yCoords2)
+        
+        file.close()
+
         
 class ImgProcessor():
     def __init__(self):
         self.cam = VideoCapture(0)
         self.thresh = (64,128,192)
         self.blurAmount = 7
-        takePicture()
-        processPictures()
+        self.colorPalette = [[0,165,255],[255,118,72],[128,0,0]]
         
+        self.takePicture()
+        self.processPicture()
+    
+    def camAutoAdjust(self):    #let the camera auto-adjust itself by taking a bunch of pictures
+        cnt = 0
+        while cnt < 10:
+            r, f = self.cam.read()
+            cnt += 1
+    
     def takePicture(self):
-        ret, frame = cam.read()
+        ret, frame = self.cam.read()
         self.frameCrop = frame[:,135:504,:]
         self.imColor = np.zeros_like(frame_crop)
-        processPictures()
+        self.processPicture()
         
-    def processPictures(self):
+    def processPicture(self):
         self.imBlur = cv2.cvtColor(self.frameCrop, cv.CV_BGR2GRAY)
         self.imBlur = cv2.medianBlur(imBlur,self.blurAmt)
         self.imPost = np.copy(self.imBlur)
@@ -173,9 +230,9 @@ class ImgProcessor():
         self.imPost = cv2.morphologyEx(self.imPost,cv2.MORPH_CLOSE,kernel)
 
         imColor = np.zeros_like(frame_crop)
-        imColor[imPost == 0] = colorPalette[2]
-        imColor[imPost == 85] = colorPalette[1]
-        imColor[imPost == 170] = colorPalette[0]
+        imColor[imPost == 0] = self.colorPalette[2]
+        imColor[imPost == 85] = self.colorPalette[1]
+        imColor[imPost == 170] = self.colorPalette[0]
         imColor[imPost == 255] = [255,255,255]
 
         self.displayImage = cv2.resize(imColor, (291,379))
@@ -183,135 +240,47 @@ class ImgProcessor():
     def getDisplayImage(self):
         return self.displayImage
         
-    def getDisplayImage(self):
+    def getPaintImage(self):
         return self.imPost
         
     def setKnobs(self, A, B, C):
         temp = A
-        if B < A:
+        if B < A:   #check A and B and make A lower
             temp = B
             B = A
             A = temp
-        if C < temp2:
-            temp3 = temp2
-            temp2 = C
-        if temp2 < temp1:
-           
+        if C < B:   #check B and C and make B lower
+            temp = C
+            C = B
+            B = temp
+            if B < A:   #if need be, check the new B and A
+                temp = B
+                B = A
+                A = temp
         
-        self.thresh =
+        self.thresh = (A, B, C)
     
-        
-        
-        
 
-#cv2.namedWindow("original")
-cv2.namedWindow("posterized")
-#cv2.namedWindow("morphed")
-vc = cv2.VideoCapture(0)
-cv.CreateTrackbar("A","posterized",192,255,nothing)
-cv.CreateTrackbar("B","posterized",128,255,nothing)
-cv.CreateTrackbar("C","posterized",64,255,nothing)
-
-strKey = "-1"
-#kernel = np.array( [[0,0,0,1,1,1,0,0,0],
-#            [0,0,1,1,1,1,1,0,0],
-#            [0,1,1,1,1,1,1,1,0],
-#            [1,1,1,1,1,1,1,1,1],
-#            [1,1,1,1,1,1,1,1,1],
-#            [1,1,1,1,1,1,1,1,1],
-#            [0,1,1,1,1,1,1,1,0],
-#            [0,0,1,1,1,1,1,0,0],
-#            [0,0,0,1,1,1,0,0,0]] , dtype = np.uint8)
-
-
-colorPalette = [[0,165,255],[255,118,72],[128,0,0]]
-
-if vc.isOpened():
-    rval, frame = vc.read()
-else:
-    rval = False
-
-while rval:
-    aBar = cv2.getTrackbarPos("A","posterized")
-    bBar = cv2.getTrackbarPos("B","posterized")
-    cBar = cv2.getTrackbarPos("C","posterized")
-    blurAmt = 7#11
-
-    if aBar < bBar:
-        tempBar = aBar
-        aBar = bBar
-        bBar = tempBar
-    if aBar < cBar:
-        tempBar = aBar
-        aBar = cBar
-        cBar = tempBar
-    if bBar < cBar:
-        tempBar = bBar
-        bBar = cBar
-        cBar = tempBar
-        
-    frame_crop = frame[:,135:504,:] #crop the width to get the same
-        # aspect ratio as our canvas
-        #assumes that the frame starts as 480 x 640
-
-    imCopy = np.copy(frame_crop)
-    imColorize = np.zeros_like(frame_crop)
+def main():
+    imProc = ImgProcessor()
+    myTracer = Tracer()
     
-    imGray = cv2.cvtColor(imCopy, cv.CV_BGR2GRAY)
-
-    imBlur = cv2.medianBlur(imGray,blurAmt)
+    imProc.camAutoAdjust()
+    imProc.takePicture()
     
-    imPost = np.copy(imBlur)
-    imPost[imPost >= aBar] = 255
-    imPost[(imPost >= bBar) & (imPost < aBar)] = 170
-    imPost[(imPost >= cBar) & (imPost < bBar)] = 85
-    imPost[imPost < cBar]  = 0
-
-
-    cv2.putText(frame, strKey, (400,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, cv.Scalar(255,255,255), 1)
-
-    
-    imMorphed = cv2.morphologyEx(imPost,cv2.MORPH_OPEN,kernel)
-    imMorphed = cv2.morphologyEx(imMorphed,cv2.MORPH_CLOSE,kernel)
-
-    imColorize[imMorphed == 0] = colorPalette[2]
-    imColorize[imMorphed == 85] = colorPalette[1]
-    imColorize[imMorphed == 170] = colorPalette[0]
-    imColorize[imMorphed == 255] = [255,255,255]
-
-    imResized = cv2.resize(imColorize, (291,379))
-
-    #cv2.imshow("original", frame)
-    #cv2.imshow("posterized",imPost)
-    #cv2.imshow("morphed",imMorphed)
-    cv2.imshow("posterized",imResized)
-    rval, frame = vc.read()
-    key = cv2.waitKey(30)
-    if key != -1:
-        key = key & 0xff
-        strKey = str(key)
-
-    if key == 27: #ESC
-        break
-    if key == 32: #this is space... bar
-        imageStrEnd = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d-%H%M%S') + ".png"
-        imBin1 = np.zeros_like(imMorphed)
-        imBin2 = np.zeros_like(imMorphed)
-        imBin3 = np.zeros_like(imMorphed)
+    while True:
+        cv2.imshow("test",imProc.getDisplayImage())
         
-        imBin1[imMorphed == 170] = 255
-        imBin2[imMorphed == 85] = 255
-        imBin3[imMorphed == 0] = 255
+        key = cv2.waitKey(30)
+        if key != -1:
+            key = key & 0xff
 
-        cv2.imwrite(("./binIm/layer1_"+imageStrEnd),imBin1)
-        cv2.imwrite(("./binIm/layer2_"+imageStrEnd),imBin2)
-        cv2.imwrite(("./binIm/layer3_"+imageStrEnd),imBin3)
-        
-#        cv2.imwrite(("picOrig"+imageStrEnd), imCopy)
-#        cv2.imwrite(("picGray"+imageStrEnd), imGray)
-#        cv2.imwrite(("picBlur"+imageStrEnd), imBlur)
-#        cv2.imwrite(("picPoster"+imageStrEnd), imPost)
-#        cv2.imwrite(("picColor"+imageStrEnd), imColorize)
+        if key == 27: #ESC
+            break
+            
+        if key == 32: #this is space... bar
+            imProc.takePicture()
 
-    if key == 10: #enter
-        
+        if key == 10: #enter
+            myTracer.trace(imProc.getPaintImage())
+            myTracer.writeFile('../MATLAB/pythonOutput3.txt')
